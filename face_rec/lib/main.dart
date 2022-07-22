036f875e8d782103a621f77fdd783a853af0611d
13531272585 <chenpeiming@primedigitaltech.com>
2022-07-22 14:56:38 +0800

    add sign up page

---
+++ face_rec/lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:face_rec/login_page.dart';
+import 'package:face_rec/signup_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      routes: <String, WidgetBuilder>{
+        LoginPage.tag: (context) => const LoginPage(),
+        SignUpPage.tag: (context) => const SignUpPage(),
+      },
+    );
+  }
+}
