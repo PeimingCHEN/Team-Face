466c16281ebd9fab8887cfede4748700a64ef223
13531272585 <chenpeiming@primedigitaltech.com>
2022-07-23 18:18:18 +0800

    update models and apis

--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,30 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import (
+from rest_framework import status
+from django.http import Http404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from accounts.serializers import (
+    OrganizationListSerializer,
     OrganizationSerializer,
+    UserListSerializer,
     UserSerializer,
-    UserFaceImageSerializer,
-    InvitationCodeSerializer
 )
-from .models import (
+from accounts.models import (
     Organization,
     User,
-    UserFaceImage,
-    InvitationCode
 )
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.views import APIView
-from django.http import Http404
 
 
-# Create your views here.
-@api_view(['GET','POST'])
-def get_routes(request):
-    routes = {}
-    return Response(routes)
-
-
+@permission_classes([IsAuthenticated])
 class organization_list_apiview(APIView):
-
     def get(self, request, format=None):
         organizations = Organization.objects.all()
-        serializer = OrganizationSerializer(organizations, many=True)
-        return Response(serializer.data, safe=False)
+        serializer = OrganizationListSerializer(organizations, many=True)
+        return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = OrganizationSerializer(data=JSONParser().parse(request))
+        serializer = OrganizationListSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,
@@ -42,7 +32,8 @@ class organization_list_apiview(APIView):
         return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
 
-    
+
+@permission_classes([IsAuthenticated])
 class organization_apiview(APIView):
     def get_object(self, name):
         try:
@@ -58,7 +49,7 @@ class organization_apiview(APIView):
     def put(self, request, name, format=None):
         organization = self.get_object(name)
         serializer = OrganizationSerializer(organization,
-                                        data=JSONParser().parse(request))
+                                        data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -71,15 +62,15 @@ class organization_apiview(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+@permission_classes([IsAuthenticated])
 class user_list_apiview(APIView):
-
     def get(self, request, format=None):
         users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, safe=False)
+        serializer = UserListSerializer(users, many=True)
+        return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = UserSerializer(data=JSONParser().parse(request))
+        serializer = UserListSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,
@@ -87,74 +78,76 @@ class user_list_apiview(APIView):
         return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
 
+
+@permission_classes([IsAuthenticated])
 class user_apiview(APIView):
-    def get_object(self, name):
+    def get_object(self, phone):
         try:
-            return User.objects.get(name=name)
+            return User.objects.get(phone=phone)
         except User.DoesNotExist:
             raise Http404
 
-    def get(self, request, name, format=None):
-        user = self.get_object(name)
+    def get(self, request, phone, format=None):
+        user = self.get_object(phone)
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
-    def put(self, request, name, format=None):
-        user = self.get_object(name)
+    def put(self, request, phone, format=None):
+        user = self.get_object(phone)
         serializer = UserSerializer(user,
-                                        data=JSONParser().parse(request))
+                                    data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+                        status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, name, format=None):
-        user = self.get_object(name)
+    def delete(self, request, phone, format=None):
+        user = self.get_object(phone)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class invitationcode_list_apiview(APIView):
+# class invitationcode_list_apiview(APIView):
 
-    def get(self, request, format=None):
-        invitations = InvitationCode.objects.all()
-        serializer = InvitationCodeSerializer(invitations, many=True)
-        return Response(serializer.data, safe=False)
+#     def get(self, request, format=None):
+#         invitations = InvitationCode.objects.all()
+#         serializer = InvitationCodeSerializer(invitations, many=True)
+#         return Response(serializer.data, safe=False)
 
-    def post(self, request, format=None):
-        serializer = InvitationCodeSerializer(data=JSONParser().parse(request))
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,
-                                status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request, format=None):
+#         serializer = InvitationCodeSerializer(data=JSONParser().parse(request))
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,
+#                                 status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,
+#                             status=status.HTTP_400_BAD_REQUEST)
 
-class invitationcode_apiview(APIView):
-    def get_object(self, code):
-        try:
-            return InvitationCode.objects.get(code=code)
-        except InvitationCode.DoesNotExist:
-            raise Http404
+# class invitationcode_apiview(APIView):
+#     def get_object(self, code):
+#         try:
+#             return InvitationCode.objects.get(code=code)
+#         except InvitationCode.DoesNotExist:
+#             raise Http404
 
-    def get(self, request, code, format=None):
-        invitation_code = self.get_object(code)
-        serializer = InvitationCodeSerializer(invitation_code)
-        return Response(serializer.data)
+#     def get(self, request, code, format=None):
+#         invitation_code = self.get_object(code)
+#         serializer = InvitationCodeSerializer(invitation_code)
+#         return Response(serializer.data)
 
-    def put(self, request, code, format=None):
-        invitation_code = self.get_object(code)
-        serializer = InvitationCodeSerializer(invitation_code,
-                                        data=JSONParser().parse(request))
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, code, format=None):
+#         invitation_code = self.get_object(code)
+#         serializer = InvitationCodeSerializer(invitation_code,
+#                                         data=JSONParser().parse(request))
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors,
+#                             status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, code, format=None):
-        invitation_code = self.get_object(code)
-        invitation_code.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, code, format=None):
+#         invitation_code = self.get_object(code)
+#         invitation_code.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
