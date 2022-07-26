33a007b8eeb5713e71d1ce984cd62249755fbff5
蔡礼华 <clh@primedigitaltech.com>
2022-07-26 18:35:04 +0800

    fix bugs in siamese model

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.core.files.storage import FileSystemStorage
 from backend.settings import MEDIA_ROOT, MEDIA_URL
 
-# INVITATION_CODE_TYPE = [
-#     ('限时', '限时'),
-#     ('一次性', '一次性')
-# ]
 
 image_storage = FileSystemStorage(location=MEDIA_ROOT,
                                     base_url=MEDIA_URL)
@@ -27,19 +23,6 @@ class Organization(models.Model):
         auto_now_add=True, verbose_name='注册时间')
     update_time = models.DateField(
         auto_now=True, verbose_name='更新时间')
-
-
-# class InvitationCode(models.Model):
-
-#     code = models.CharField(primary_key=True,max_length=128)
-#     organization = models.ForeignKey(Organization,on_delete=models.CASCADE)
-#     code_type = models.CharField(max_length=128,choices=INVITATION_CODE_TYPE,default="一次性")
-#     property = models.IntegerField(default=1)
-#     use_status = models.BinaryField(default=False)
-#     created_time = models.DateField(auto_now_add=True,
-#                                     verbose_name=u'注册时间')
-#     update_time = models.DateField(auto_now=True,
-#                                     verbose_name=u'更新时间')
 
 
 class User(models.Model):
@@ -67,12 +50,21 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+
+def user_directory_path(instance, filename):
+    return 'user/{}/{}'.format(instance.user.phone, filename)
+
+
 class UserFaceImage(models.Model):
     class Meta:
         verbose_name = '用户头像照片'
         verbose_name_plural = '用户头像照片'
+
+
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE,
                              related_name='images')
     image = models.ImageField(
-        upload_to='user/', storage=image_storage, verbose_name='头像照片')
+        upload_to=user_directory_path, storage=image_storage, verbose_name='头像照片')
+
+    
